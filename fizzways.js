b3195327e1f9d93e2828cc901d549ba48b7d6796
shubham kalamkar <shubhamkalamkar0101@gmail.com>
2023-04-13 17:44:08 +0530

    first

---
+++ fizzways.js
@@ -0,0 +1,20 @@
+const FizzBuzz = (mobile)=>{
+    var s = "";
+    sum = mobile
+        .toString()
+        .split('')
+        .map(Number)
+        .reduce(function (a, b) {
+            return a + b;
+        },0);
+        console.log(sum)
+
+for (var i = 1; i <= sum; i++) {
+     if (i % 4 == 0) s += "Fizz" + " ";
+    else if (i % 5 == 0) s += "Buzz" + " ";
+    else if (i % 4 == 0 && i % 5 == 0) s += "FizzBuzz" + " ";
+    else s += i + " ";
+}
+return s;
+}
+console.log(FizzBuzz(9552530101))
